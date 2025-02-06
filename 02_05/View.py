3ac5de59c94e5072e72dbc0ba8156f95c69527c1
Ian <ijae0611@gmail.com>
2025-02-06 15:17:24 +0900

    modified

--- 02_05/View.py
+++ 02_05/View.py
@@ -34,26 +34,39 @@ for test_case in range(1, T + 1):
 N = 11
 building = [0, 0, 118, 90, 243, 178, 99, 100, 200, 0, 0]
 
+# def open_sight(building, N):
+#     count = 0
+
+#     for idx in range(2, N-2):                                         # 양쪽 2칸 씩 제외 (탐색 불가)
+#         my_building = building[idx]                                   # 현재 건물의 높이 
+#         left_building = max(building[idx-1], building[idx-2])         # 왼쪽 2칸 내 건물 중 가장 높은 건물의 높이
+#         right_building = max(building[idx+1], building[idx+2])        # 오른쪽 2칸 내 건물 중 가장 높은 건물의 높이
+
+#         left_sight = 0                                                # 조망권이 확보된 층 (왼쪽, 오른쪽)
+#         right_sight = 0
+
+#         if left_building < my_building:                               # 왼쪽, 오른쪽 2칸 중 가장 높은 건물이 현재 건물보다 작으면
+#             left_sight = my_building - left_building                  # 그 차이만큼의 층 수는 한 쪽 조망권이 확보 
+
+#         if right_building < my_building:
+#             right_sight = my_building - right_building
+
+#         count += min(left_sight, right_sight)                         # 조망권은 높은 층 부터 확보되므로 왼쪽 오른쪽 중 적은 층 수가 양쪽 조망권 다 확보 
+
+#     return count                                                      # 탐색하면서 전부 저장 
+
+
 def open_sight(building, N):
+
     count = 0
 
-    for idx in range(2, N-2):                                         # 양쪽 2칸 씩 제외 (탐색 불가)
-        my_building = building[idx]                                   # 현재 건물의 높이 
-        left_building = max(building[idx-1], building[idx-2])         # 왼쪽 2칸 내 건물 중 가장 높은 건물의 높이
-        right_building = max(building[idx+1], building[idx+2])        # 오른쪽 2칸 내 건물 중 가장 높은 건물의 높이
+    for idx in range(2, N-2):
 
-        left_sight = 0                                                # 조망권이 확보된 층 (왼쪽, 오른쪽)
-        right_sight = 0
+        max_building = max(building[idx-2], building[idx-1], building[idx+1], building[idx+2])
 
-        if left_building < my_building:                               # 왼쪽, 오른쪽 2칸 중 가장 높은 건물이 현재 건물보다 작으면
-            left_sight = my_building - left_building                  # 그 차이만큼의 층 수는 한 쪽 조망권이 확보 
+        if building[idx] > max_building:
+            count += building[idx] - max_building
 
-        if right_building < my_building:
-            right_sight = my_building - right_building
-
-        count += min(left_sight, right_sight)                         # 조망권은 높은 층 부터 확보되므로 왼쪽 오른쪽 중 적은 층 수가 양쪽 조망권 다 확보 
-
-    return count                                                      # 탐색하면서 전부 저장 
+    return count
 
 print(open_sight(building, N))
-
