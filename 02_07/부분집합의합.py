60d9879eda7f2645432493bd2f61547f0ad010b3
Ian <ijae0611@gmail.com>
2025-02-07 12:18:34 +0900

    subset

--- 02_07/부분집합의합.py
+++ 02_07/부분집합의합.py
@@ -1,24 +1,35 @@
 # https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AZTP1QzqXnbHBIRD&contestProbId=AZEGAQUa-sgDFAVs&probBoxId=AZTP3wYKXqXHBIRD&type=USER&problemBoxTitle=%2802.07%29+List2-1&problemBoxCnt=8
 
-'''
-N, K = int(input().split())
+import sys
+sys.stdin = open("sample_input.txt", "r")
 
-'''
-N = 3
-K = 6
 
-set = [1, 2, 3, 4]
-bit = [0, 0, 0, 0]
+def count_subset(num_element, sum_element):
+    my_set = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    cnt = 0
 
-for i in range(2):    # 각 원소의 포함 여부 할당
-    bit[0] = i
-    for j in range(2):
-        bit[1] = j
-        for k in range(2):
-            bit[2] = k
-            for l in range(2):
-                bit[3] = l
+    for i in range(1 << 12):                 # 2^12가지 부분집합 전부 탐색
+        bits = [0] * 12                      # 반복할 때 마다 비트 초기화
+        sum_sub = 0                          # 부분집합 원소의 합 초기화
 
-                for m in range(4):
-                    if bit[m] == 1:
-                        print(set[m], end = "")
+        for j in range(12):
+            if i & (1 << j):
+                bits[j] += 1                 # 부분집합의 비트 기록
+
+        if sum(bits) == num_element:         # 비트 개수가 입력과 일치하는 경우
+            for k in range(12):
+                if bits[k] == 1:             # 비트가 1인 인덱스 찾기
+                    sum_sub += my_set[k]     # 동일한 인덱스를 가지는 집합의 원소 누적합
+
+        if sum_sub == sum_element:           # 부분집합의 원소의 합이 입력과 일치하면 카운트
+            cnt += 1
+
+    return cnt
+
+
+T = int(input())
+
+for t in range(1, T+1):
+    N, K = map(int, input().split())
+
+    print(f"#{t} {count_subset(N, K)}")
