ed588f8056898918572abb913f9969c19a3273e6
Ian <ijae0611@gmail.com>
2025-02-10 22:27:21 +0900

    fin

--- 02_10/달팽이 숫자/1954.py
+++ 02_10/달팽이 숫자/1954.py
@@ -8,30 +8,32 @@ def snail_matrix(size):
     snail = [[0]*size for _ in range(size)]     # 0만 들어있는 달팽이 생성
     row, col = 0, 0
     number = 1
-    snail[row][col] += number
 
-    for number in range(1, size**2+1):
-        snail[row][col] += number
+    snail[row][col] += number                   # 출발점에서 번호 더하고 시작
 
-        if 0 <= col + 1 < size:
-            if snail[row][col + 1] == 0:
-                col += 1
-                continue
+    while number < size**2:                     # number가 최댓값에 도달할 때 까지
+    # 다음 향할 항목의 인덱스가 범위 내에 있고 비어 있으면 좌표 이동, 숫자 + 1한 뒤 숫자 할당
+        while col + 1 < size and snail[row][col+1] == 0:
+            col += 1
+            number += 1
+            snail[row][col] += number
 
-        if 0 <= row + 1 < size:
-            if snail[row + 1][col] == 0:
-                row += 1
-                continue
+        while row + 1 < size and snail[row+1][col] == 0:
+            row += 1
+            number += 1
+            snail[row][col] += number
 
-        if 0 <= col - 1 < size:
-            if snail[row][col - 1] == 0:
-                col -= 1
-                continue
+        while col - 1 >= 0 and snail[row][col-1] == 0:
+            col -= 1
+            number += 1
+            snail[row][col] += number
 
-        if 0 <= row - 1 < size:
-            if snail[row - 1][col] == 0:
-                row -= 1
-                continue
+        while row - 1 >= 0 and snail[row-1][col] == 0:
+            row -= 1
+            number += 1
+            snail[row][col] += number
+
+        continue
 
     for s in snail:
         print(" ".join(map(str, s)))
