01acd3bf5822566cf204813407bc483893fae8c1
Ian <ijae0611@gmail.com>
2025-02-10 16:48:22 +0900

    still

--- 02_10/달팽이 숫자/1954.py
+++ 02_10/달팽이 숫자/1954.py
@@ -3,3 +3,42 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
+
+def snail_matrix(size):
+    snail = [[0]*size for _ in range(size)]     # 0만 들어있는 달팽이 생성
+    row, col = 0, 0
+    number = 1
+    snail[row][col] += number
+
+    for number in range(1, size**2+1):
+        snail[row][col] += number
+
+        if 0 <= col + 1 < size:
+            if snail[row][col + 1] == 0:
+                col += 1
+                continue
+
+        if 0 <= row + 1 < size:
+            if snail[row + 1][col] == 0:
+                row += 1
+                continue
+
+        if 0 <= col - 1 < size:
+            if snail[row][col - 1] == 0:
+                col -= 1
+                continue
+
+        if 0 <= row - 1 < size:
+            if snail[row - 1][col] == 0:
+                row -= 1
+                continue
+
+    for s in snail:
+        print(" ".join(map(str, s)))
+
+
+T = int(input())
+for t in range(1, T+1):
+    N = int(input())
+    print(f"#{t}")
+    snail_matrix(N)
