0c6dcc33a5dc549ffc5819dad6a19b618f06c9a8
Ian <ijae0611@gmail.com>
2025-02-11 09:30:21 +0900

    fix

--- 02_10/달팽이 숫자/1954.py
+++ 02_10/달팽이 숫자/1954.py
@@ -8,13 +8,13 @@ def snail_matrix(size):
     snail = [[0]*size for _ in range(size)]     # 0만 들어있는 달팽이 생성
     row, col = 0, 0
     number = 1
-<<<<<<< HEAD
-=======
 
     snail[row][col] += number                   # 출발점에서 번호 더하고 시작
 
     while number < size**2:                     # number가 최댓값에 도달할 때 까지
-    # 다음 향할 항목의 인덱스가 범위 내에 있고 비어 있으면 좌표 이동, 숫자 + 1한 뒤 숫자 할당
+
+        # 다음 향할 항목의 인덱스가 범위 내에 있고 비어 있으면 좌표 이동, 숫자 + 1한 뒤 숫자 할당
+
         while col + 1 < size and snail[row][col+1] == 0:
             col += 1
             number += 1
