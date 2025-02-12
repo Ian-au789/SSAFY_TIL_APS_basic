4fdbb8864b43df5f5aba6b60ba018115f2a0f77e
Ian <ijae0611@gmail.com>
2025-02-12 16:21:39 +0900

    pattern

---
+++ 02_12/string/1213.py
@@ -0,0 +1,16 @@
+# https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AZTP1QzqXnbHBIRD&contestProbId=AV14P0c6AAUCFAYi&probBoxId=AZTP3wYKXqjHBIRD&type=PROBLEM&problemBoxTitle=%2802.12%29+String-2&problemBoxCnt=4
+
+import sys
+sys.stdin = open('test_input.txt', encoding='utf-8')
+
+
+def how_many_pattern(pattern, sentence):
+    pass
+
+
+for t in range(10):
+    T = int(input())
+    p = input()
+    str_input = input()
+
+    print(f"{T} {how_many_pattern(p, str_input)}")
