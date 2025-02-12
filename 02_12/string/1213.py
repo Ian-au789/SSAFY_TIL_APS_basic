74e15db64b70ae99f0930d3caafd399f065d219a
Ian <ijae0611@gmail.com>
2025-02-12 17:25:01 +0900

    boyer

--- 02_12/string/1213.py
+++ 02_12/string/1213.py
@@ -4,8 +4,28 @@ import sys
 sys.stdin = open('test_input.txt', encoding='utf-8')
 
 
-def how_many_pattern(pattern, sentence):
-    pass
+def boyer_moore(pattern, sentence):
+    len_sen = len(sentence)
+    len_pat = len(pattern)
+    cnt = 0
+    idx = len_pat - 1
+    check_list = list(pattern)[::-1]                                  # 인덱스 계산 편하게 하기 위해 거꾸로
+
+    while idx <= len_sen - 1:
+        for i in range(len_pat):
+            if sentence[idx - i] != check_list[i]:                    # 문자열의 항목이 패턴의 항목과 일치하나 확인
+
+                if sentence[idx - i] in check_list[i:]:               # 불일치한 항목이 현 위치 앞 패턴 항목 중 있을 때
+                    idx += check_list[i:].index(sentence[idx - i])    # 그 위치로 이동
+                else:
+                    idx += len_pat                                    # 부분 일치가 없으므로 이동
+                break
+
+            else:
+                if i == len_pat - 1:                                  # 패턴이 일치하면 카운트 하고 패턴 길이 만큼 이동
+                    cnt += 1
+                    idx += len_pat
+    return cnt
 
 
 for t in range(10):
@@ -13,4 +33,4 @@ for t in range(10):
     p = input()
     str_input = input()
 
-    print(f"{T} {how_many_pattern(p, str_input)}")
+    print(f"#{T} {boyer_moore(p, str_input)}")
