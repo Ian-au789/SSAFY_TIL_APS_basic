e19489475f83e2088cc6fbe666b9ecabf2e91d2d
Ian <ijae0611@gmail.com>
2025-02-12 11:09:43 +0900

    02/12

--- 02_12/백만장자/1859.py
+++ 02_12/백만장자/1859.py
@@ -5,17 +5,25 @@ sys.stdin = open("input.txt", "r")
 
 
 def max_profit(future, prices):
+    size = len(prices)
     profit = 0
 
-    for _ in range(future):
-        prices.append(0)
+    now_max = max(prices[1:future])                     # 탐색 전 최댓값 확인
 
-    first_max = max(prices[1:future])
+    for i in range(size-1):
+        if i <= size-future:                            # 탐색하는 범위가 리스트의 마지막에 도달할 때 까지
+            now_max = max(now_max, prices[i+future-1])  # 새로 탐색 범위에 들어오는 항목과 현재의 최댓값 비교해서 갱신
+            if prices[i] < now_max:
+                profit += now_max - prices[i]           # 오늘의 가격과 미래의 최대 가격의 차이 만큼 이윤 발생
 
-    for i in range(len(prices)-future):
-        now_max = max(first_max, prices[i+future-1])
-        if prices[i] < now_max:
-            profit += now_max - prices[i]
+            elif prices[i] == now_max:
+                now_max = max(prices[i+1:i+future])     # 최댓값이 갱신되지 않고 탐색 범위를 벗어나면 최댓값 다시 구하기
+
+        else:
+            if prices[i] < now_max:                     # 탐색할 항목이 더 늘어나지 않음
+                profit += now_max - prices[i]
+            elif prices[i] == now_max:                  # 최댓값이 탐색 범위를 떠날 때만 고려
+                now_max = max(prices[i+1:size])
 
     return profit
 
