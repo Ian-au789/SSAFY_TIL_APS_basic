15e93cc46e536abb11c9abafacf1496b5e08f15f
jiwon <jiwnee252@gmail.com>
2025-02-14 17:02:46 +0900

    still

--- 02_14/그래프 경로/21648.py
+++ 02_14/그래프 경로/21648.py
@@ -4,45 +4,49 @@ import sys
 sys.stdin = open("4871_input.txt")
 
 
-def graph_edge(num_v, num_e, v_s, v_e, edge_list):
-
+def route_exist(num_v, v_start, v_end):
+    # 스택 준비
     top = -1
-    stack = [""]*num_e
-    visited = [""]*num_v
+    stack = [0]*num_v
+    visited = [0]*(num_v+1)
 
+    # 시작 노드 저장
     top += 1
-    stack[top] = v_s
-    visited[0] = v_s
+    stack[top] = v_start                          # 현재 위치하고 있는 노드
+    visited[v_start] = 1                          # 방문한 적 있는 노드는 1로 체크
+    last = 0
 
-    while top >= 0:
+    while 1:
+        for v_next in edges[stack[top]]:          # 해당 노드와 이어진 경로 탐색
+            if v_next == v_end:                   # 다음 노드가 종착점과 일치하면 1 반환
+                return 1
 
-        for idx in range(num_e):
-            if stack[top] in edge_list[idx]:
+            elif visited[v_next] == 0:            # 다음 노드가 아직 방문하지 않은 곳이면
+                top += 1
+                stack[top] = v_next               # 해당 노드로 이동
+                visited[v_next] = 1               # 방문했다고 체크
+                last += 1
 
-                for v in edge_list[idx]:
-                    if v not in visited and v != stack[top]:
-                        top += 1
-                        stack[top] = v
+            else:                                 # 둘 다 아니라면 더 탐색
+                last = 0
+                continue
 
-                        if stack[top] == v_e:
-                            return 1
+        if last == 0:
+            top -= 1
 
-                        break
-
-            if idx == num_e - 1:
-                top -= 1
-
-    return 0
+            if top == -1:                         # 시작점으로 돌아오면 0 반환 (경로가 없음)
+                return 0
 
 
 T = int(input())
 for t in range(1, T+1):
     V, E = map(int, input().split())
-    input_list = []
+    edges = [[] for _ in range(V+1)]
 
     for _ in range(E):
         v1, v2 = map(int, input().split())
-        input_list.append([v1, v2])
+        edges[v1].append(v2)                     # v1노드를 인덱스 번호로 가지는 항목에 v2 저장
+        edges[v2].append(v1)                     # 반대의 경로도 입력 (경로는 양방향)
 
     start, end = map(int, input().split())
-    print(f"#{t} {graph_edge(V, E, start, end, input_list)}")
+    print(f"#{t} {route_exist(V, start, end)}")
