a09432645c3ed81776dbca2d49ff7bcb33a74708
Ian <ijae0611@gmail.com>
2025-02-16 01:01:38 +0900

    stick

--- 02_14/쇠막대기 자르기/5432.py
+++ 02_14/쇠막대기 자르기/5432.py
@@ -5,10 +5,30 @@ sys.stdin = open("sample_input.txt")
 
 
 def cut_stick(brackets):
-    pass
+    size = len(brackets)
+    top = -1
+    idx = 0
+    cnt = 0
+
+    while idx < size:
+        if brackets[idx] == "(" and brackets[idx+1] == ")":        # 레이저 () 탐색
+            cnt += top+1                                           # 레이저가 현재 위치를 지나가는 막대기들을 자르면서 그 개수만큼 막대기 추가
+            idx += 2                                               # 2칸 이동
+
+        else:
+            if brackets[idx] == "(":                               # 쇠막대 왼쪽 끝 탐색
+                top += 1
+                idx += 1
+                cnt += 1                                           # 막대기 카운트
+
+            else:                                                  # 쇠막대 오른쪽 끝 탐색
+                top -= 1
+                idx += 1
+
+    return cnt
 
 
 T = int(input())
 for t in range(1, T+1):
-    string = input()
-    print(f"#{t} {cut_stick(string)}")
+    input_list = list(map(str, input()))
+    print(f"#{t} {cut_stick(input_list)}")
