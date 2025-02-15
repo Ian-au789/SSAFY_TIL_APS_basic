4fe17672a5781650456e14f30477884076874e85
Ian <ijae0611@gmail.com>
2025-02-15 22:36:40 +0900

    stack-graph

--- 02_14/그래프 경로/21648.py
+++ 02_14/그래프 경로/21648.py
@@ -14,24 +14,24 @@ def route_exist(num_v, v_start, v_end):
     top += 1
     stack[top] = v_start                          # 현재 위치하고 있는 노드
     visited[v_start] = 1                          # 방문한 적 있는 노드는 1로 체크
-    last = 0
 
     while 1:
         for v_next in edges[stack[top]]:          # 해당 노드와 이어진 경로 탐색
-            if v_next == v_end:                   # 다음 노드가 종착점과 일치하면 1 반환
-                return 1
 
-            elif visited[v_next] == 0:            # 다음 노드가 아직 방문하지 않은 곳이면
+            if visited[v_next] == 0:              # 다음 노드가 아직 방문하지 않은 곳이면
+
+                if v_next == v_end:               # 다음 노드가 종착점과 일치하면 1 반환
+                    return 1
+
                 top += 1
                 stack[top] = v_next               # 해당 노드로 이동
                 visited[v_next] = 1               # 방문했다고 체크
-                last += 1
+                break
 
-            else:                                 # 둘 다 아니라면 더 탐색
-                last = 0
+            else:
                 continue
 
-        if last == 0:
+        else:
             top -= 1
 
             if top == -1:                         # 시작점으로 돌아오면 0 반환 (경로가 없음)
@@ -46,7 +46,8 @@ for t in range(1, T+1):
     for _ in range(E):
         v1, v2 = map(int, input().split())
         edges[v1].append(v2)                     # v1노드를 인덱스 번호로 가지는 항목에 v2 저장
-        edges[v2].append(v1)                     # 반대의 경로도 입력 (경로는 양방향)
 
     start, end = map(int, input().split())
     print(f"#{t} {route_exist(V, start, end)}")
+
+# for-else 문 쓰면 더 쉽다.
