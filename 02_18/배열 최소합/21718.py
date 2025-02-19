9559fc2ca9b00a0e038b2dc5a17f9651a742031d
Ian <ijae0611@gmail.com>
2025-02-19 12:39:17 +0900

    Prunning

--- 02_18/배열 최소합/21718.py
+++ 02_18/배열 최소합/21718.py
@@ -4,32 +4,38 @@ import sys
 sys.stdin = open("4881_input.txt")
 
 
-def f(i, size):
-    if i == size:
-        result = 0
-        idx = 0
-        for n in perm:
-            result += input_matrix[idx][n]
-            idx += 1
-        all_sum.append(result)
+def find_min_sum(level, size, mid_sum):
+    global min_sum
+
+    if level == size:                                        # 마지막 레벨에 도달했을 때
+        if mid_sum < min_sum:                                # 현재 최소합보다 작으면 갱신
+            min_sum = mid_sum
+        return
+
+    if mid_sum > min_sum:                                    # 중간합이 이미 현재 최소합을 넘어버리면 탐색 중단 (가지치기)
+        return
 
     else:
-        for j in range(i, N):
-            perm[i], perm[j] = perm[j], perm[i]
-            f(i + 1, N)
-            perm[i], perm[j] = perm[j], perm[i]
+        for i in range(size):
+            if not visited[i]:                               # 아직 방문하지 않은 열
+                visited[i] = 1                               # 방문 기록
+                mid_sum += matrix[level][i]                  # 현재 주소의 항목 더하기
+                find_min_sum(level + 1, size, mid_sum)       # 그 다음 행으로 넘어가서 재귀 호출
+                mid_sum -= matrix[level][i]                  # 돌아 왔으면 더했던 값 제거
+                visited[i] = 0                               # 방문 기록 초기화
 
 
 T = int(input())
 for t in range(1, T+1):
     N = int(input())
-    input_matrix = []
+    matrix = []
     for _ in range(N):
         input_list = list(map(int, input().split()))
-        input_matrix.append(input_list)
+        matrix.append(input_list)
 
-    perm = [i for i in range(N)]
-    all_sum = []
+    visited = [0] * N
+    min_sum = sum([matrix[i][i] for i in range(N)])            # 최소 합 초기값 설정
 
-    f(0, N)
-    print(f"#{t} {min(all_sum)}")
+    find_min_sum(0, N, 0)
+
+    print(f"#{t} {min_sum}")
--- 연습장.py
+++ 연습장.py
@@ -7,12 +7,27 @@ for t in range(1, T+1):
     for _ in range(N):
         input_list = list(map(int, input().split()))
         input_matrix.append(input_list)
-'''
+
 
 # 한 줄 입력받기
-'''
+
 T = int(input())
 for t in range(1, T+1):
     N = int(input)
     input_list = list(map(int, input().split()))
 '''
+
+
+def f(i, N):  # 크기가 N이고 순열을 저장한 p배열에서 p[i]를 결정하는 함수
+    if i == N:  #
+        print(p)
+    else:
+        for j in range(i, N):
+            p[i], p[j] = p[j], p[i]
+            f(i + 1, N)  # i+1자리 결정
+            p[i], p[j] = p[j], p[i]
+
+
+p = [1, 2, 3]
+N = 3
+f(0, N)
