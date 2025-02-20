5ebbe289badc9268b8529d18fb07ac83cfb0317f
Ian <ijae0611@gmail.com>
2025-02-20 18:17:17 +0900

    finish

--- Stack/02_18/계산기2/1223.py
+++ Stack/02_18/계산기2/1223.py
@@ -1,1 +1,39 @@
+# https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AZTP1QzqXnbHBIRD&contestProbId=AV14nnAaAFACFAYD&probBoxId=AZTP3wYKXqzHBIRD&type=PROBLEM&problemBoxTitle=%2802.18%29+Stack2-2&problemBoxCnt=5
 
+import sys
+sys.stdin = open("input.txt")
+
+def calculator(size, string):
+    stack = [""] * size
+    top = -1
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+    for n in string:
+        if n in numbers:
+            top += 1
+            stack[top] = int(n)
+            if stack[top - 1] == "*":
+                mult = stack[top - 2] * stack[top]
+                top -= 2
+                stack[top] = mult
+        else:
+            top += 1
+            stack[top] = n
+
+    while top > 0:
+        if stack[top] == "+":
+            added = stack[top -1] + stack[top + 1]
+            top -= 1
+            stack[top] = added
+
+        else:
+            top -= 1
+
+    return stack[top]
+
+
+
+for t in range(1, 11):
+    N = int(input())
+    expression = input()
+    print(f"#{t} {calculator(N, expression)}")
--- Stack/02_18/줄 서는 방법.py
+++ Stack/02_18/줄 서는 방법.py
@@ -1,24 +1,45 @@
 # https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AZTP1QzqXnbHBIRD&contestProbId=AZE0RIBqwTsDFAVs&probBoxId=AZTP3wYKXqzHBIRD&type=USER&problemBoxTitle=%2802.18%29+Stack2-2&problemBoxCnt=5
 
-import sys
-sys.stdin = open("4881_input.txt")
+def factorial(n):
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
 
 
 def permutation(n, k):
-    global cnt
-    numbers = [i for i in range(1, n+1)]
-    size = len(numbers)
+    numbers = list(range(1, n + 1))  # 1부터 n까지 숫자 리스트
+    answer = []
+    k -= 1  # 인덱스 보정 (0부터 시작)
 
-    if n == 0:
-        cnt += 1
-        if cnt == k:
-            return numbers
+    for i in range(n, 0, -1):
+        fact = factorial(i - 1)  # 직접 구현한 팩토리얼 함수 사용
+        index = k // fact  # 현재 위치에서 사용할 숫자의 인덱스
+        answer.append(numbers.pop(index))  # 해당 숫자 선택 후 리스트에서 제거
+        k %= fact  # 다음 선택을 위해 k 업데이트
 
-    else:
-        for j in (size - k, size):
-            numbers[j], numbers[size - n] = numbers[size - n], numbers[j]
-            permutation(n-1, k)
-            numbers[j], numbers[size - n] = numbers[size - n], numbers[j]
+    return answer
 
 
-cnt = 0
+# Math 라이브러리 사용 방법
+from math import factorial
+
+
+def permutation_math(n, k):
+    numbers = list(range(1, n + 1))  # 1부터 n까지 숫자 리스트
+    answer = []
+    k -= 1  # 인덱스 보정 (0부터 시작)
+
+    for i in range(n, 0, -1):
+        fact = factorial(i - 1)  # (i-1)! 값
+        index = k // fact  # 현재 위치에서 사용할 숫자의 인덱스
+        answer.append(numbers.pop(index))  # 해당 숫자 선택 후 리스트에서 제거
+        k %= fact  # 다음 선택을 위해 k 업데이트
+
+    return answer
+
+
+
+print(permutation(3, 6))  # [3, 2, 1]
+print(permutation(4, 9))  # [2, 3, 1, 4]
+print(permutation(5, 16)) # [3, 1, 5, 2, 4]
