c3fd20c62cc6a6f4ec571cfafbe49b39a3df5d73
Ian <ijae0611@gmail.com>
2025-02-21 00:08:52 +0900

    ceremony

--- Stack/02_18/자기 방으로 돌아가기/4408.py
+++ Stack/02_18/자기 방으로 돌아가기/4408.py
@@ -5,16 +5,23 @@ sys.stdin = open("sample_input.txt")
 
 
 def back_to_room(size, matrix):
-    pass
+
+    if size == 0:
+        return 0
+
+    else:
+        pass
+
 
 
 T = int(input())
 for t in range(1, T+1):
     N = int(input())
     rooms = []
+    visited = [0] * 400
 
     for _ in range(N):
         room_info = list(map(int, input().split()))
-        rooms.append(room_info)
+        rooms.append([room_info[0]//2, room_info[1]//2 - 1])      # 해당 방 번호의 인덱스 위치
 
     print(f"#{t} {back_to_room(N, rooms)}")
