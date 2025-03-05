319aab916acd1494d4b7ec1275eb205cc8fc0d99
Ian <ijae0611@gmail.com>
2025-03-06 00:24:09 +0900

    class

--- 03_04/이진탐색/23883.py
+++ 03_04/이진탐색/23883.py
@@ -4,25 +4,15 @@ import sys
 sys.stdin = open("5176_input.txt")
 
 
+class Binary_Tree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
 T = int(input())
 for t in range(1, T+1):
     N = int(input())
-    n = N//2
-    numbers = [n for n in range(1, N+1)]
-    root = numbers[n]
 
-    queue = [[root, numbers]]
-    n = N
-    n -= 1
-    while n > 0:
-        queue.append([queue[0][1][len(queue[0][1][:len(queue[0][1]) // 2 - 1]) // 2], queue[0][1][:len(queue[0][1]) // 2]])
-        n -= 1
-        if n == 0:
-            break
-        queue.append([queue[0][1][queue[0][0] + len(queue[0][1][queue[0][0]:]) // 2], queue[0][1][queue[0][0]:]])
-        n -= 1
-        queue.pop(0)
-
-    result = queue.pop()
-
-    print(f"#{t} {root} {result[0]}")
+    print(f"#{t} {} {}")
