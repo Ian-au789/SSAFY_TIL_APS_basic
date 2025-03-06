dcaa20a635289b0a89e84cd52d1b746c93f407ef
Ian <ijae0611@gmail.com>
2025-03-06 09:25:44 +0900

    done

--- 03_05/노드의 합/23886.py
+++ 03_05/노드의 합/23886.py
@@ -5,20 +5,23 @@ sys.stdin = open("5178_input.txt")
 
 
 T = int(input())
-for t in range(1, T+1):
+for t in range(1, T + 1):
     N, M, L = map(int, input().split())
     matrix = [list(map(int, input().split())) for _ in range(M)]
 
-    bin_tree = [0]*(N+1)
+    bin_tree = [0] * (N + 1)  # 이진트리 저장할 배열
 
     for row in matrix:
-        bin_tree[row[0]] = row[1]
+        bin_tree[row[0]] = row[1]  # 리프 노드 저장
 
-    idx = N
+    idx = N  # 마지막 리프 노드부터 시작
 
-    if N % 2 == 1:
+    if N % 2 == 0:  # 만약 마지막 리프노드가 부모노드와 혼자 연결되어 있다면 혼자 반영
+        bin_tree[idx // 2] = bin_tree[idx]
+        idx -= 1
 
+    while idx > 1:
+        bin_tree[idx // 2] = bin_tree[idx] + bin_tree[idx - 1]  # 부모노드 = 자식노드 둘의 합
+        idx -= 2
 
-    else:
-        bin_tree[idx//2] = bin_tree[idx]
-
+    print(f"#{t} {bin_tree[L]}")
